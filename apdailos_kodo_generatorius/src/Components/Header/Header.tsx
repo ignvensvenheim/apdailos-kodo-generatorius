4056b9bc712d54e80a3517b43e95a8bf530e27b2
ignvensvenheim <ignas.venckunas@gmail.com>
2025-05-06 08:54:37 +0300

    page reload on logo click

--- apdailos_kodo_generatorius/src/Components/Header/Header.tsx
+++ apdailos_kodo_generatorius/src/Components/Header/Header.tsx
@@ -2,7 +2,13 @@ import "./header.css";
 import logo from "../../assets/Svenheim_logo_rgb.webp";
 
 function Header() {
-  return <img src={logo} alt="svenheim logo" />;
+  return (
+    <img
+      src={logo}
+      alt="svenheim logo"
+      onClick={() => window.location.reload()}
+    />
+  );
 }
 
 export default Header;
